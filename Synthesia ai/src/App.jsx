02b34437e8dbf7a6b71433180a11920d8525fd57
Hammad Ul Hassan <108016049+hammadulhassan123@users.noreply.github.com>
2024-01-29 01:48:53 +0500

    done till signup

---
+++ Synthesia ai/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import './App.css'
+import Navbar from './components/Navbar'
+import Hero from './components/Overview/Hero'
+import Intro from './components/Overview/Intro'
+import Login from './components/Login/Login'
+
+function App() {
+
+  return (
+    <>
+      <Navbar/>
+    </>
+  )
+}
+
+export default App
---
+++ Synthesia ai/src/components/Overview/Overview.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Intro from './Intro'
+import Hero from './Hero'
+
+const Overview = () => {
+  return (
+    <>
+        <Hero/>
+        <Intro/> 
+    </>
+  )
+}
+
+export default Overview
